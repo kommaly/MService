c262767ddadb5249529cf59005dc5547b9cbd0bf
kommaly <kommaly_pge@yahoo.com>
2016-03-24 11:02:06 +0700

    Initial Commit

---
+++ MServices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MServices
+//
+//  Created by Kommaly  on 3/24/2559 BE.
+//  Copyright © 2559 LTC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
